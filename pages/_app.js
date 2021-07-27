1ed2ea2f8d593828b7168396dba3b479426636e0
Jacin Fleta de Castro <jacintofleta@gmail.com>
2021-07-27 09:53:54 +0200

    Progress bar in the top of the page when switching pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,41 @@
 import "tailwindcss/tailwind.css";
 import { Provider } from "next-auth/client";
+import NProgress from "nprogress";
+
+import Head from "next/head";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.events.on("routeChangeComplete", (url) => {
+      NProgress.done();
+    });
+
+    router.events.on("routeChangeStart", () => {
+      NProgress.start();
+    });
+
+    router.events.on("routeChangeError", () => {
+      NProgress.done();
+    });
+    return () => {
+      router.events.off("routeChangeComplete");
+    };
+  }, [router.events]);
+
   return (
-    <Provider session={pageProps.session}>
-      <Component {...pageProps} />
-    </Provider>
+    <>
+      <Head>
+        {/* Import CSS for nprogress */}
+        <link rel="stylesheet" href="/nprogress.css" />
+      </Head>
+      <Provider session={pageProps.session}>
+        <Component {...pageProps} />
+      </Provider>
+    </>
   );
 }
 
